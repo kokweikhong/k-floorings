c4aa981675de5abdd1f87d3d5f24bab718adf856
kokweikhong <kokweikhong@gmail.com>
2023-05-12 16:18:50 +0800

    convert js to ts

---
+++ app/contact/layout.tsx
@@ -0,0 +1,10 @@
+export const metadata = {
+  title:
+    "Start Your Timber Wood Project | Contact Us in Malaysia and Singapore",
+  description:
+    "Ready to start your timber wood project? Contact our expert team at Calvary Carpentry in Malaysia and Singapore today to get started. We offer ",
+};
+
+export default function RootLayout({ children }) {
+  return <>{children}</>;
+}
