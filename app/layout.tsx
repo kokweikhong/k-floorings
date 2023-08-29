bba4a8f647c4da2fcdf87fbbaa284ca66c79d975
kokweikhong <kokweikhong@gmail.com>
2023-08-29 11:32:21 +0800

    add gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "../styles/main.css";
+import Script from "next/script";
 import { Inter, Raleway } from "@/utils/fonts";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -9,6 +10,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${Inter.variable} ${Raleway.variable}`}>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-L9RKKW4JLP" />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-L9RKKW4JLP');
+        `}
+        </Script>
         <ProductProvider>
           <Header />
           {children}
